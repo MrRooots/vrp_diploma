259c16585260acc38966e16690e470a32371b021
MrRooots <mrrooots.dev@gmail.com>
2023-05-04 19:46:31 +0500

    Web version improvements

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,7 @@
-/** Load table of inputs for given sizes */
+/**
+ * Load table of inputs for given sizes
+ * @param { int } size
+ */
 function loadTable(size) {
   const table = document.getElementById('graphTable');
   table.innerHTML = '';
@@ -6,7 +9,7 @@ function loadTable(size) {
   const header = table.createTHead();
   const headerRow = header.insertRow(0);
   for (let i = 0; i < size + 1; i++) {
-    headerRow.insertCell(i).innerHTML = i === 0 ? '' : `${i}`;
+    headerRow.insertCell(i).innerHTML = i === 0 ? '' : `${i - 1}`;
   }
 
 
@@ -14,8 +17,17 @@ function loadTable(size) {
     const tableRow = table.insertRow();
 
     for (let j = 0; j < size + 1; j++) {
-      if (j === 0) tableRow.insertCell(j).innerHTML = `${i + 1}`;
-      else tableRow.insertCell(j).innerHTML = '<input type="text">';
+      if (j === 0) {
+        tableRow.insertCell(j).innerHTML = `${i}`;
+      } else {
+        tableRow.insertCell(j).innerHTML = `
+          <input type="number"
+                 onfocus="this.style.backgroundColor='initial'"
+                 onkeyup="inputOnChange(this, ${i}, ${j - 1}, ${size})" 
+                 ${i === j - 1 ? 'value="0" readonly' : ''}
+                 ${i < j - 1 ? 'tabindex=0' : 'tabindex=-1'}>
+        `;
+      }
     }
   }
 
@@ -41,6 +53,14 @@ function getGraphContent() {
 
 /** Request for VRP problem solution */
 function solveVRP() {
+  const inputs = document.querySelectorAll('input:not(#graphPointsCount)');
+  for (const input of inputs) {
+    if (input.value === '') {
+      input.style.backgroundColor = 'lightcoral';
+      return;
+    }
+  }
+
   const xhr = new XMLHttpRequest();
 
   xhr.open('POST', '/solve');
@@ -48,11 +68,9 @@ function solveVRP() {
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.send(
-    JSON.stringify(
-      {
-        points: getGraphContent()
-      }
-    )
+    JSON.stringify({
+      points: getGraphContent()
+    })
   );
 
   xhr.onreadystatechange = function (response) {
@@ -65,4 +83,21 @@ function solveVRP() {
       answerContainer.innerHTML += `<h3>The shortest path length is: ${responseJson['length']}</h3>`
     }
   }
+}
+
+/**
+ * Fill symmetric table field then input content changes
+ * @param { Object } instance
+ * @param { int } i
+ * @param { int } j
+ * @param { int } size
+ */
+function inputOnChange(instance, i, j, size) {
+  const inputs = document.querySelectorAll('input:not(#graphPointsCount)');
+
+  inputs.forEach(
+    (input) => input.getAttribute('name') !== 'size'
+      ? inputs[size * j + i].value = instance.value
+      : null
+  )
 }
