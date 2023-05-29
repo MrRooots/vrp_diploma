e22043a567947f431ed14614c3f6d14ad4421b08
MrRooots <mrrooots.dev@gmail.com>
2023-05-29 22:49:39 +0500

    generation: nodes positions

--- src/core/utils/graph_generator.py
+++ src/core/utils/graph_generator.py
@@ -1,3 +1,6 @@
+import ast
+import json
+import os
 from src.core.structures.graph import Graph
 
 
@@ -15,4 +18,13 @@ class GraphGenerator:
   def from_file(filename: str) -> Graph:
     """ Generate `Graph` from given file """
     with open(f'data/input/{filename}', 'r') as file:
-      return Graph(matrix=[[float(v) for v in line.split()] for line in file])
+      matrix = [
+        [float(v) if v != '0' else None for v in line.split()]
+        for line in file
+      ]
+      layout = None
+      if os.path.isfile(f'data/input/layouts/layout_{filename}'):
+        with open(f'data/input/layouts/layout_{filename}', 'r') as f:
+          layout = ast.literal_eval(f.readline())
+
+    return Graph(matrix, layout)
